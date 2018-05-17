2123a2e422c52073d3e0a23155a7f2ab97f2e256
Kester Allen <kallen@amyris.com>
2018-05-17 15:42:28 -0700

    ENH: OCR!

---
+++ scripts/ocr.sh
@@ -0,0 +1,15 @@
+
+set -x
+
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 image_url"
+  exit 1
+fi
+
+key=$(< ../key.txt)
+json="{ \"requests\": [ { \"features\": [ { \"type\": \"TEXT_DETECTION\" } ], \"image\": { \"source\": { \"imageUri\": \"$1\" } } } ] }"
+echo $json
+curl -s -H "Content-Type: application/json" "https://vision.googleapis.com/v1/images:annotate?key=$key" --data "$json"
+
