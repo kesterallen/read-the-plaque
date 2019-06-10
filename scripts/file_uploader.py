8a93b08191478ea33b01a10d130e469fb77e7cb2
Kester Allen <kester@gmail.com>
2019-06-09 20:10:53 -0700

    REF: Improvement to clarity of exif lat/lng conversion

--- scripts/file_uploader.py
+++ scripts/file_uploader.py
@@ -6,6 +6,8 @@ import requests
 
 from PIL import Image
 
+DEBUG = False
+
 # Values in these constants was extracted from:
 # from PIL.ExifTags import TAGS, GPSTAGS
 GPS_INFO_TAG = 34853 # "GPSInfo"
@@ -13,8 +15,6 @@ LAT_REF = 1 # e.g. "N"
 LAT = 2 # e.g. ((37, 1), (50, 1), (2381, 100))
 LNG_REF = 3 # e.g. W
 LNG = 4 # e.g. ((122, 1), (17, 1), (2834, 100))
-
-DEBUG = True
 
 class Plaque:
     """
@@ -45,6 +45,7 @@ class Plaque:
                 'lng': self.lng,
                 'title': self.title,
                 'description': self.description,
+                #'tags': 'bench',
             }
             response = requests.post(Plaque.submit_url, files=files, data=data)
             response.raise_for_status()
@@ -52,19 +53,24 @@ class Plaque:
     def _set_exif_lat_lng(self):
         """Extract the location info from the image tags, using exiftool"""
 
-        def _exif_to_deg(exif, ref):
-            degs, mins, secs = [float(exif[i][0]) for i in range(3)]
-            latorlng = degs + mins / 60.0 + secs / 3600.0
+        def _degs_min_secs_from_exif(exif):
+            degs, mins, secs_times_100 = [float(exif[i][0]) for i in range(3)]
+            secs = secs_times_100 / 100.0 # ??? not sure where this comes from
+            return degs, mins, secs
+
+        def _decimal_pos_from_exif(exif, ref):
+            degs, mins, secs = _degs_min_secs_from_exif(exif)
+            decimal = degs + mins / 60.0 + secs / 3600.0
             if ref in ('S', 'W'):
-                latorlng *= -1
-            return latorlng
+                decimal *= -1
+            return decimal
 
         try:
             with Image.open(self.fname) as img:
                 exif = img._getexif()
                 info = exif[GPS_INFO_TAG]
-                self.lat = _exif_to_deg(info[LAT], info[LAT_REF])
-                self.lng = _exif_to_deg(info[LNG], info[LNG_REF])
+                self.lat = _decimal_pos_from_exif(info[LAT], info[LAT_REF])
+                self.lng = _decimal_pos_from_exif(info[LNG], info[LNG_REF])
         except TypeError as err:
             print("typeerr", err)
             self.lat = self.DEFAULT_LAT
@@ -79,17 +85,19 @@ def main(img_fnames):
     The Plaque.
     """
     plaques = []
+    print("Loading {} images".format(len(img_fnames)))
     for fname in img_fnames:
         plaque = Plaque(fname)
         plaques.append(plaque)
+    print("Loading complete")
 
     for i, plaque in enumerate(plaques):
         try:
             if not DEBUG:
                 plaque.submit()
-            print("Uploaded {}/{} {}".format(i+1, len(plaques), plaque))
+            print("Posted {}/{} {}".format(i+1, len(plaques), plaque))
         except requests.exceptions.HTTPError as err:
-            print("upload for {} failed, {}".format(plaque, err))
+            print("post for {} failed, {}".format(plaque, err))
         if not DEBUG:
             time.sleep(10)
 
