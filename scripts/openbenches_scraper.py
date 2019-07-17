117162bc0bf17a8e98539566733689a172ebec4a
Kester Allen <kester@gmail.com>
2019-07-17 10:53:27 -0700

    Switch to cache images to reduce download bandwidth, use https://creativecommons.org/licenses/by-sa/4.0/ as default license.

--- scripts/openbenches_scraper.py
+++ scripts/openbenches_scraper.py
@@ -8,9 +8,12 @@ import time
 from attrdict import AttrDict
 import requests
 
-DEBUG = True
-MAX_CHAR_LENGTH = 60
+DEBUG = False
+
+MAX_CHAR_LENGTH = 80
 URL_PREFIX = 'https://openbenches.org'
+CACHE_URL = 'https://arhklxrfen.cloudimg.io/width/1024/webp/openbenches.org/image/{}'
+DEF_LICENSE = 'https://creativecommons.org/licenses/by-sa/4.0/'
 COMMON_FIRST_LINES = set([
     'donated by',
     'donated in loving memory of',
@@ -26,26 +29,28 @@ COMMON_FIRST_LINES = set([
     'to the memory of',
 ])
 
-
 class NoImageException(Exception):
     """Exception to throw for no-image benches"""
 
 class SkipBenchException(Exception):
     """Exception to throw for benches to skip (e.g. ReadThePlaque)"""
 
-
 def _shorten_title_if_needed(title, max_len):
     """Make title shorter if it's too long"""
-    if len(title) < max_len:
-        out = title
-    else:
+    if len(title) > max_len:
         words = title.split()
         out = ''
         for word in words:
-            out += " " + word
             if len(out) >= max_len:
                 break
-    return out
+            out += " " + word
+        title = out
+
+    # Remove unicode, etc:
+    printable = set(string.printable)
+    title = "".join(filter(lambda x: x in printable, title)).strip()
+
+    return title
 
 def _get_title(bench):
     """Exgtract titletitle shorter if it's too long"""
@@ -54,27 +59,52 @@ def _get_title(bench):
 
     lines = [l for l in title.splitlines() if l]
     if not lines:
-        # No description
+        # No description, use a default:
         title = "OpenBenches Plaque"
-    elif len(title) < MAX_CHAR_LENGTH:
-        # Short description, use entire title
-        pass
     elif lines[0].lower() in COMMON_FIRST_LINES:
-        # Long description, but the first line is "In memory of" or
-        # something else common, so append the second line to it
+        # The first line is "In memory of" or something else common, so grab
+        # the first two lines:
         title = " ".join(lines[:2])
     else:
-        # Long, uncommon description, grab the first line, shorten if
-        # necessary:
-        title = _shorten_title_if_needed(lines[0], MAX_CHAR_LENGTH)
+        # Uncommon description, use the first line:
+        title = lines[0]
 
-    # Clean title of unicode, etc
-    printable = set(string.printable)
-    title = "".join(filter(lambda x: x in printable, title)).strip()
+    title = _shorten_title_if_needed(title, MAX_CHAR_LENGTH)
     return title, full_title
 
 def bench_to_plaque(bench, user_names):
     """Convert a bench's JSON record to a plaque dict"""
+
+    def _get_desc():
+        # Build description, including image metadata: original source,
+        # license, and user:
+        desc = [
+            """ {0.full_title} <br/><br/> This plaque is originally from
+            <a href="https://openbenches.org/bench/{0.id}">OpenBenches</a> and
+            is imported with their permission <br/> """.format(plaque)
+        ]
+        if 'importURL' in first_img:
+            url = first_img['importURL']
+            if url.startswith('https://readtheplaque.com'):
+                raise SkipBenchException("Skipping for RTP")
+            desc.append('Original location: <a href="{}">here</a>'.format(url))
+
+        # Default license per Terence Eden DM 2019-07-16 to @readtheplaque
+        img_license = 'under <a href="{}">CC by SA 4.0</a>'.format(DEF_LICENSE)
+        # N.B. British spelling of key
+        if 'licence' in first_img:
+            img_license = first_img['licence'] 
+        desc.append('The image and text is licensed {}'.format(img_license))
+
+        if 'user' in first_img:
+            user_id = first_img['user']
+            if user_id in user_names:
+                url = 'https://openbenches.org/user/{}'.format(user_id)
+                name = user_names[user_id]
+                desc.append('<p>Uploaded by <a href="{}">{}</a>'.format(
+                    url, name))
+
+        return "<br>".join(desc)
 
     title, full_title = _get_title(bench)
     # If there isn't an image, bail out:
@@ -90,38 +120,14 @@ def bench_to_plaque(bench, user_names):
         'lng': coords[0],
         'lat': coords[1],
         'plaque_image_url': URL_PREFIX + first_img['URL'],
-        #('http://lh3.googleusercontent.com/5tJoII-uCRkJJAdh7fKeGw6Z_RGT78PGCrxauOlnWxCnD37XLiLAtbnblQdSMC5ZTVOhRmr0B7VNbJbMlilHxs4npuMX9Q=s4096'),
         'tags': 'openbenches.org',
+
+        'has_lic': False,
+        'has_user': False,
+        'has_origin': False,
     })
 
-    # Image metadata: original source, license, user:
-    desc = [
-        """
-        {0.full_title}
-        <br/><br/>
-        This plaque is originally from
-        <a href="https://openbenches.org/bench/{0.id}">OpenBenches</a> and is
-        imported with their permission
-        <br/>
-        """.format(plaque)
-    ]
-    if 'importURL' in first_img:
-        url = first_img['importURL']
-        if url.startswith('https://readtheplaque.com'):
-            raise SkipBenchException("Skipping for RTP")
-        desc.append('Original location: <a href="{}">here</a>'.format(url))
-    if 'license' in first_img:
-        img_license = first_img['license']
-        desc.append('The image and is licensed {}'.format(img_license))
-        #TODO: default license?
-    if 'user' in first_img:
-        user_id = first_img['user']
-        if user_id in user_names:
-            url = 'https://openbenches.org/user/{}'.format(user_id)
-            name = user_names[user_id]
-            desc.append('<p>Uploaded by <a href="{}">{}</a>'.format(url, name))
-
-    plaque.description = "<br>".join(desc)
+    plaque.description = _get_desc()
     return plaque
 
 def load_benches(fname, users_fname):
@@ -150,24 +156,31 @@ def load_benches(fname, users_fname):
 
     return plaques
 
+def cache_url(img_url):
+    """Get the image from the cheaper-bandwidth cache"""
+    fname = img_url.split('/')[-1]
+    return CACHE_URL.format(fname)
+
 def main():
     """Run the uploader"""
     host = 'http://localhost:8080' if DEBUG else 'https://readtheplaque.com'
     submit_url = '{}/add'.format(host)
 
     plaques = load_benches('openbenches.json', 'openbenches.users.json')
-    if DEBUG:
-        plaques = [random.choice(plaques) for i in range(5)]
-
-    for i, plaque in enumerate(plaques):
+    for i, plaque in enumerate(random.sample(plaques, 5)):
         try:
-            response = requests.post(submit_url, data=plaque)
-            response.raise_for_status()
+            plaque['plaque_image_url'] = cache_url(plaque['plaque_image_url'])
+            if not DEBUG:
+                response = requests.post(submit_url, data=plaque)
+                response.raise_for_status()
+
             msg = "Posted {}/{} {}".format(i+1, len(plaques), plaque.title)
             print(msg)
         except requests.exceptions.HTTPError as err:
-            print("post for {} failed, {}".format(plaque, err))
-        time.sleep(10)
+            print("post for \n{}\n failed,\n {}\n".format(plaque, err))
+
+        if not DEBUG:
+            time.sleep(10)
 
 if __name__ == '__main__':
     main()
