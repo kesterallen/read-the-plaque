7164e3ab36159d55b48366822bb791c9a1dbe4cf
Kester Allen <kester@gmail.com>
2022-07-12 13:55:53 -0700

    ENH: Allow updates of the GEOJSON file from the site's updated location. Fix daylight savings time bug.

--- scripts/update_geojson_map_file.py
+++ scripts/update_geojson_map_file.py
@@ -5,19 +5,17 @@ import json
 import os
 import sys
 
+from pytz import timezone
 import dateutil.parser
 import requests
 
 GEOJSON_URL = "https://readtheplaque.com/geojson"
-ALL_PLAQUES_DATE = "2000-01-01 12:34:56.7"
+ALL_PLAQUES_DATE = "2000-01-01 12:34:56.7"  # way before the first plaque was added
 
-here_path = os.path.dirname(os.path.abspath(__file__))
-geojson_file = os.path.join(here_path, "../static/plaques.geojson")
+HERE_PATH = os.path.dirname(os.path.abspath(__file__))
+GEOJSON_FILE = os.path.join(HERE_PATH, "../static/plaques.geojson")
 
 ALL = False
-# UTC_OFFSET = 8 # PST
-UTC_OFFSET = 7  # PDT
-UTC_TIMEDELTA = datetime.timedelta(hours=UTC_OFFSET)
 
 
 def print_status(tmpl, num_plaques):
@@ -25,8 +23,12 @@ def print_status(tmpl, num_plaques):
     suffix = "" if num_plaques == 1 else "s"
     print(tmpl.format(num_plaques, suffix))
 
+    num_updates = len(sys.argv) - 1
+    suffix2 = "" if num_updates == 1 else "s"
+    print(f"Updated {num_updates} location{suffix2} in plaques.geojson")
 
-def offset_time(last_updated_str):
+
+def time_to_utc(last_updated_str):
     """
     This shift into UTC time seems to be necessary. The GAE datastore shows
     the times recorded in the Plaque entities are in "PDT". As of this writing,
@@ -35,24 +37,27 @@ def offset_time(last_updated_str):
     Unclear to me why this is required. I think that something in the process
     is assuming dates are in UTC, but I don't see where.
 
-    Anyhow:
-        INPUTS:
-            A string representing a date that the dateutil parser can consume.
-        RETURNS:
-            A string representing a date UTC_OFFSET later.
+    INPUTS:
+        A string representing a date that the dateutil parser can consume.
+    RETURNS:
+        A string representing a date utc_offset later than the input.
 
-    This'll break every time change.
+    Ref: https://stackoverflow.com/questions/17173298/is-a-specific-timezone-using-dst-right-now
     """
+    zone = "America/Los_Angeles"
+    is_dst = datetime.datetime.now(tz=timezone(zone)).dst()
+    utc_offset = 7 if is_dst else 8  # PST vs PDT
+
     last_updated = dateutil.parser.parse(last_updated_str)
-    offset_forward = last_updated + UTC_TIMEDELTA
-    return offset_forward.strftime("%Y-%m-%d %H:%M:%S.%f")
+    utc_time = last_updated + datetime.timedelta(hours=utc_offset)
+    return utc_time.strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
 def _get_plaques_geojson(updated_on, tmpl):
     """
     Get the geojson representation of plaques published since updated_on.
     """
-    updated_on = offset_time(updated_on)
+    updated_on = time_to_utc(updated_on)
     resp = requests.post(GEOJSON_URL, data={"updated_on": updated_on})
     geojson = json.loads(resp.content.decode("utf-8"))
 
@@ -77,10 +82,10 @@ def add_new_plaques():
 
     # Get the plaques that are already in the geojson file, and extract the
     # update timestamp:
-    with open(geojson_file) as json_fh:
+    with open(GEOJSON_FILE, encoding="utf8") as json_fh:
         existing_geojson = json.load(json_fh)
 
-    # Get plaques from the site that have been added since the late update:
+    # Get plaques from the site that have been added since the last update:
     updated_on = existing_geojson["updated_on"]
     geojson = _get_plaques_geojson(updated_on, "Found {} new plaque{}.")
 
@@ -91,20 +96,50 @@ def add_new_plaques():
     return existing_geojson
 
 
+def fix_json_location(url):
+    """
+    Update the Big Map entry for a plaque in the static/plaques.geojson file
+    with the location data from the website.
+    """
+    resp = requests.get(f"{GEOJSON_URL}/{url}")
+    if resp.status_code != 200:
+        print(f"no plaque at {url}")
+        return
+    geojson = resp.json()
+    lng, lat = geojson["geometry"]["coordinates"]
+
+    with open(GEOJSON_FILE, encoding="utf8") as json_fh:
+        existing_geojson = json.load(json_fh)
+
+    for plaque in existing_geojson["features"]:
+        if f"/plaque/{url}" == plaque["properties"]["title_page_url"]:
+            old = plaque["geometry"]["coordinates"]
+            plaque["geometry"]["coordinates"] = [lng, lat]
+            print(f"updated {url} to from {old} to {lng}, {lat}")
+
+    geojson_str = json.dumps(existing_geojson)
+    with open(GEOJSON_FILE, "w", encoding="utf8") as json_fh:
+        json_fh.write(geojson_str)
+
+
 def main():
     """Update the Big Map geojson file"""
-    # TODO
-    #plaque_urls = [u for u in sys.argv[1:]] if len(sys.argv) > 1 else None
-    #print(plaque_urls)
 
-    # Get existing plaques from json file, add new plaques convert to geojson
-    # and write that to the .geojson file
+    # Update a plaques entry in the Big Map static/plaques.geojson file for one
+    # or more plaques, given as URLs (e.g. four-citizens-killed-by-british-soldiers
+    # for the plaque at # readtheplaque.com/plaque/four-citizens-killed-by-british-soldiers),
+    # if any URLs are specified on the command line.
+    if len(sys.argv) > 1:
+        for fix_json_location_url in sys.argv[1:]:
+            fix_json_location(fix_json_location_url)
+
+    # Get the list of existing plaques from json file, and add new plaques:
     geojson_plaques = get_all_plaques() if ALL else add_new_plaques()
-    geojson_plaques["updated_on"] = str(datetime.datetime.now())
 
-    # Update geojson RTP file
+    # Write out the updated geojson file with a new timestamp:
+    geojson_plaques["updated_on"] = str(datetime.datetime.now())
     geojson_str = json.dumps(geojson_plaques)
-    with open(geojson_file, "w") as json_fh:
+    with open(GEOJSON_FILE, "w", encoding="utf8") as json_fh:
         json_fh.write(geojson_str)
 
 
