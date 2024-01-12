55a4a9d07813b6298f63732a5bf69f22116258a2
Kester Allen <kester@gmail.com>
2024-01-12 09:32:36 -0800

    About/RSS/map/tag/pending pages

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Main run script
 
 import datetime as dt
 from google.cloud import ndb
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 from rtp.models import Plaque, FeaturedPlaque
@@ -13,6 +13,8 @@ FIRST_YEAR = 2015
 FIRST_MONTH = 9
 FIRST_DAY = 9
 
+DEF_NUM_PENDING = 5
+DEF_NUM_RSS = 10
 DEF_NUM_PER_PAGE = 25
 DEF_RAND_NUM_PER_PAGE = 5
 
@@ -24,28 +26,35 @@ DEF_RAND_NUM_PER_PAGE = 5
 
 app = Flask(__name__)
 
+# TODO: get cursor or next button working
+
+
+def get_key(key_filename="key_googlemaps.txt"):
+    """Get the value of the key in the given file"""
+    with open(key_filename) as key_fh:
+        key = key_fh.read().rstrip()
+    return key
+
 
 # TODO: get users accounts working
 def _loginout() -> dict:
     return dict(isadmin=False, url=None, text=None)
 
 
-@app.route("/")
-def all_plaques():
+@app.route("/", methods=["GET", "HEAD"])
+def many_plaques():
+    """View a page of multiple plauqes"""
+    # Return a lightweight response for a HEAD request
+    if request.method == "HEAD":
+        return render_template("head.html")
+
     # cursor = None # TODO
     per_page = DEF_NUM_PER_PAGE
     # is_random = False # TODO
     # is_featured = True # TODO
 
-    client = ndb.Client()
-    with client.context() as context:
-        plaques = (
-            Plaque.query()
-            .filter(Plaque.approved == True)
-            .order(-Plaque.created_on)
-            .fetch(limit=per_page)
-        )
-
+    with ndb.Client().context() as context:
+        plaques, next_cur, more = Plaque.fetch_page(DEF_NUM_PER_PAGE)
         return render_template(
             "all.html",
             plaques=plaques,
@@ -54,24 +63,31 @@ def all_plaques():
         )
 
 
-@app.route("/plaque/<string:search_term>/")
+@app.route("/pending")
+@app.route("/pending/<int:num>")
+def pending_plaques(num: int = DEF_NUM_PENDING) -> str:
+    """View the most recent pending plaques."""
+    with ndb.Client().context() as context:
+        plaques = Plaque.pending_list(num)
+        return render_template(
+            "all.html",
+            plaques=plaques,
+            next_cursor_urlsafe="foo",  # TODO
+            loginout=_loginout(),
+        )
+
+
+@app.route("/plaque/<string:search_term>/", methods=["GET", "HEAD"])
 def one_plaque(search_term: str) -> str:
+    """View one plaque."""
+    # Return a lightweight response for a HEAD request
+    if request.method == "HEAD":
+        return render_template("head.html")
+
     # TODO: add other search terms possibilities (key, old ID, etc)
-    client = ndb.Client()
-    with client.context() as context:
-        one_plaque = Plaque.query().filter(Plaque.title_url == search_term).get()
-        return render_template("one.html", plaques=[one_plaque], loginout=_loginout())
-
-
-def _random_time(year=FIRST_YEAR, month=FIRST_MONTH, day=FIRST_DAY) -> dt.datetime:
-    """
-    Utilize the fact that the first plaque submission was 2015-09-09 to
-    generate a random time between then and now.
-    """
-    first = dt.datetime(year, month, day)
-    now = dt.datetime.now()
-    rand_seconds = random.randint(0, int((now - first).total_seconds()))
-    return first + dt.timedelta(seconds=rand_seconds)
+    with ndb.Client().context() as context:
+        plaque = Plaque.query().filter(Plaque.title_url == search_term).get()
+        return render_template("one.html", plaques=[plaque], loginout=_loginout())
 
 
 @app.route("/random")
@@ -79,9 +95,20 @@ def _random_time(year=FIRST_YEAR, month=FIRST_MONTH, day=FIRST_DAY) -> dt.dateti
 @app.route("/randompage")
 @app.route("/randompage/<int:num_plaques>")
 def random_plaques(num_plaques: int = 5) -> str:
+    """View a random group of plaques."""
+
+    def _random_time(year=FIRST_YEAR, month=FIRST_MONTH, day=FIRST_DAY) -> dt.datetime:
+        """
+        Utilize the fact that the first plaque submission was 2015-09-09 to
+        generate a random time between then and now.
+        """
+        first = dt.datetime(year, month, day)
+        now = dt.datetime.now()
+        rand_seconds = random.randint(0, int((now - first).total_seconds()))
+        return first + dt.timedelta(seconds=rand_seconds)
+
     plaques = []
-    client = ndb.Client()
-    with client.context() as context:
+    with ndb.Client().context() as context:
         for _ in range(num_plaques):
             plaque = (
                 Plaque.query()
@@ -93,14 +120,81 @@ def random_plaques(num_plaques: int = 5) -> str:
         return render_template("all.html", plaques=plaques, loginout=_loginout())
 
 
+# TODO: add argument for number of plaques
+@app.route("/tag/<string:tag>")
+def tagged_plaques(tag: str, only_approved: bool = True) -> str:
+    """View a group of plaques with a given tag."""
+    with ndb.Client().context() as context:
+        query = Plaque.query()
+        if only_approved:
+            query = query.filter(Plaque.approved == True)
+        plaques = (
+            query.filter(Plaque.tags == tag)
+            .order(-Plaque.created_on)
+            .fetch(limit=DEF_NUM_PER_PAGE)
+        )
+        return render_template("all.html", plaques=plaques, loginout=_loginout())
+
+
+@app.route("/map")
+@app.route("/map/<string:lat>/<string:lng>")
+@app.route("/map/<string:lat>/<string:lng>/<string:zoom>")
+def map(lat: str = None, lng: str = None, zoom: str = None) -> str:
+    """View the big map page with optional center/zoom"""
+    with ndb.Client().context() as context:
+        num_plaques = Plaque.query().filter(Plaque.approved == True).count()
+        template_values = {
+            "google_maps_api_key": get_key(),
+            "counts": num_plaques,
+            "bigmap": True,
+        }
+        if lat is not None and lng is not None:
+            template_values["bigmap_center"] = True
+            template_values["bigmap_lat"] = float(lat)
+            template_values["bigmap_lng"] = float(lng)
+
+            if zoom is not None:
+                template_values["bigmap_zoom"] = float(zoom)
+
+        return render_template("bigmap.html", **template_values)
+
+
 @app.route("/counts")
 def counts() -> str:
-    client = ndb.Client()
-    with client.context() as context:
+    """View the counts"""
+    # TODO: add verbose flag
+    with ndb.Client().context() as context:
         query = Plaque.query()
         num_plaques = query.count()
         num_pending = query.filter(Plaque.approved == False).count()
         return f"{num_plaques} plaques, {num_pending} pending"
+
+
+@app.route("/rss")
+def rss_feed() -> str:
+    """RSS feed for newly-added plaques"""
+    with ndb.Client().context() as context:
+        plaques, next_cur, more = Plaque.fetch_page(DEF_NUM_RSS)
+        return render_template("feed.xml", plaques=plaques)
+
+
+@app.route("/about")
+def about() -> str:
+    """Read the about page. This could be a static page"""
+    return render_template("about.html")
+
+
+# TODO
+# /add /submit /submit-your-own
+# /edit
+# /randpending
+# /flush
+# /nearby # TODO new API for search?
+# /geo
+# /featured /setfeatured /featured/random /featured/geojson
+# /geojson
+# /tweet
+# /alljp /updatejp /fulljp
 
 
 if __name__ == "__main__":
