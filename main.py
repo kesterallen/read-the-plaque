4edd3dd6f2237cb6042410503cd0d002789e04ea
Kester Allen <kester@gmail.com>
2024-01-16 20:39:03 -0800

    Getting close on add page

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ Main run script
 """
 
 import datetime as dt
+import os
+import re
 import random
+import urllib
 
 from google.cloud import ndb
 from flask import Flask, render_template, request, redirect
@@ -13,6 +16,8 @@ from rtp.models import Plaque, FeaturedPlaque
 FIRST_YEAR = 2015
 FIRST_MONTH = 9
 FIRST_DAY = 9
+
+DEF_PLAQUESET_NAME = "public"
 
 DEF_NUM_PENDING = 5
 DEF_NUM_RSS = 10
@@ -37,9 +42,21 @@ def get_key(key_filename="key_googlemaps.txt"):
     return key
 
 
-def _render_template(template_file, plaques=None, args_dict=None) -> str:
+# Set a parent key on the Plaque objects to ensure that they are all in the
+# same entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+#
+def get_plaqueset_key(plaqueset_name=DEF_PLAQUESET_NAME):
+    """
+    Constructs a Datastore key for a Plaque entity. Use plaqueset_name as
+    the key.
+    """
+    return ndb.Key("Plaque", plaqueset_name)
+
+
+def _render_template(template_file, plaques=None, **kwargs) -> str:
     """A wrapper for flask.render_template that injects some defaults"""
-    if args_dict is None:
+    if kwargs is None:
         return render_template(
             template_file,
             plaques=plaques,
@@ -52,7 +69,7 @@ def _render_template(template_file, plaques=None, args_dict=None) -> str:
             plaques=plaques,
             next_cursor_urlsafe="foo",  # TODO
             loginout=_loginout(),
-            **args_dict,
+            **kwargs,
         )
 
 
@@ -85,7 +102,9 @@ def pending_plaques(num: int = DEF_NUM_PENDING) -> str:
     with ndb.Client().context() as context:
         plaques = Plaque.pending_list(num)
         return _render_template(
-            "all.html", title="View Pending Plaques", plaques=plaques
+            "all.html",
+            plaques=plaques,
+            page_title="View Pending Plaques",
         )
 
 
@@ -100,58 +119,171 @@ def one_plaque(search_term: str) -> str:
     # TODO: add other search terms possibilities (key, old ID, etc)
     with ndb.Client().context() as context:
         plaque = Plaque.query().filter(Plaque.title_url == search_term).get()
+        # print("key is ", plaque.key)
         return _render_template("one.html", plaques=[plaque])
 
 
-def _get_img_from_request(request):
+def _get_img_from_request():
     """
     Get an image, either an uploaded file (prefered if both are specified), or
     from a URL pointing to a file .
     """
 
-    if img_file != "" and img_file is not None:
-        img_file = self.request.POST.get("plaque_image_file")
-        img_name = img_file.filename
-        img_fh = img_file.file
-    elif img_url != "":
-        img_url = request.form.get("plaque_image_url")
-        img_name = os.path.basename(img_url)
-        img_fh = urllib.urlopen(img_url)
+    if "plaque_image_file" in request.files:
+        img_fh = request.files["plaque_image_file"]
+        img_name = img_fh.filename
     else:
-        img_name = None
-        img_fh = None
+        img_url = request.form["plaque_image_url"]
+        img_name = os.path.basename(img_url)
+        img_fh = urllib.request.urlopen(img_url)
 
     return img_name, img_fh
 
 
-def _plaque_for_insert(args) -> Plaque:
+def _latlng_get_angles(coords_tags):
+    """
+    Convert GPS exif tag format to hours/degrees/minutes triplet
+    """
+    hours = float(coords_tags[0][0]) / float(coords_tags[0][1])
+    degrees = float(coords_tags[1][0]) / float(coords_tags[1][1])
+    minutes = float(coords_tags[2][0]) / float(coords_tags[2][1])
+    return (hours, degrees, minutes)
+
+
+def _latlng_angles_to_dec(ref, latlng_angles):
+    """Convert a degrees, hours, minutes tuple to decimal degrees."""
+    latlng = (
+        float(latlng_angles[0])
+        + float(latlng_angles[1]) / 60.0
+        + float(latlng_angles[2]) / 3600.0
+    )
+    if ref not in ["N", "E", "S", "W"]:
+        raise SubmitError(f"reference '{ref}' needs to be either N, S, E, or W")
+
+    if ref in ["S", "W"]:
+        latlng *= -1.0
+
+    return latlng
+
+
+def _get_latlng_exif(self, img_fh):
+    from PIL import Image
+    from PIL.ExifTags import TAGS, GPSTAGS
+
+    # Get GPS data out of image:
+    #
+    gps_data = {}
+    image = Image.open(img_fh)
+    info = image._getexif()
+    img_fh.seek(0)  # reset file handle
+    if info:
+        for tag, value in info.items():
+            decoded = TAGS.get(tag, tag)
+            if decoded == "GPSInfo":
+                for gps_tag in value:
+                    gps_tag_decoded = GPSTAGS.get(gps_tag, gps_tag)
+                    gps_data[gps_tag_decoded] = value[gps_tag]
+        try:
+            lat_ref = gps_data["GPSLatitudeRef"]  # N/S
+            lat_angles = _latlng_get_angles(gps_data["GPSLatitude"])
+
+            lng_ref = gps_data["GPSLongitudeRef"]  # E/W
+            lng_angles = _latlng_get_angles(gps_data["GPSLongitude"])
+
+            lat = _latlng_angles_to_dec(lat_ref, lat_angles)
+            lng = _latlng_angles_to_dec(lng_ref, lng_angles)
+        except KeyError:
+            pass  # TODO: is this right?
+
+        return lat, lng
+
+
+def _get_location(img_fh):
+    # If the location has been specified, use that:
+    lat = request.form["lat"]
+    lng = request.form["lng"]
+
+    # If it hasn't, but there's something in the search field, try that:
+    # TODO copy from AdminHandlers
+
+    # Convert to a location
+    # If that doesn't work, try to get the location from the image's EXIF info:
+    try:
+        location = ndb.GeoPt(lat, lng)
+    except ndb.exceptions.BadValueError as bve:
+        try:
+            lat, lng = self._get_latlng_exif(img_fh)
+            location = ndb.GeoPt(lat, lng)
+        except Exception as err:
+            print(err)
+            raise err
+
+    return location
+
+
+def _get_tags():
+    """Get and tokenize tags"""
+    tags_str = request.form["tags"]
+    tags_split = tags_str.split(",")
+    tags = [re.sub(r"\s+", " ", t.strip().lower()) for t in tags_split]
+    tags = [t for t in tags if t]  # Remove empties
+    return tags
+
+
+def _plaque_for_insert() -> Plaque:
     """
     Make a new Plaque object for insert
     """
-    title = self.request.get("title")[:1499]  # limit to 1500 char
-    description = self.request.get("description")
-    img_name, img_fh = _get_img_from_request(request)
-    # verbose = request.args.get("verbose", default=False, type=bool)
+    with ndb.Client().context() as context:
+        img_name, img_fh = _get_img_from_request()
 
-    plaque_search_index = search.Index(PLAQUE_SEARCH_INDEX_NAME)
-    plaque_search_index.put(plaque.to_search_document())
+        ### plaque_search_index = search.Index(PLAQUE_SEARCH_INDEX_NAME) # TODO figure out search in new GCP
+        ### plaque_search_index.put(plaque.to_search_document()) # TODO figure out search in new GCP
 
-    plaque = Plaque(parent=plaqueset_key)
-    if args.title != plaque.title:
-        plaque.title = args.title
-        plaque.set_title_url(plaqueset_key)
+        plaqueset_key = get_plaqueset_key()
+        plaque = Plaque(parent=plaqueset_key)
 
-    plaque.description = args.description
-    plaque.location = args.location
-    plaque.tags = args.tags
-    plaque.approved = False
-    plaque.created_on = datetime.datetime.now()
-    plaque.created_by = args.created_by  # ? created_by = users.get_current_user()
-    plaque.updated_on = datetime.datetime.now()
-    plaque.updated_by = None
+        if request.form["title"] != plaque.title:
+            title = request.form["title"][:1499]  # limit to 1500 char
+            plaque.title = title
+            plaque.set_title_url(plaqueset_key)
 
-    # Upload the image for a new plaque, or update the an editted plaque's image
-    self._upload_image(img_name, img_fh, plaque)
+        plaque.description = request.form["description"]
+        plaque.location = _get_location(img_fh)
+        plaque.tags = _get_tags()
+        plaque.approved = False
+        plaque.created_on = dt.datetime.now()
+        #        created_by = users.get_current_user() # TODO: figure out the users situation
+        # plaque.created_by = args.created_by  # ? created_by = users.get_current_user()
+        plaque.updated_on = dt.datetime.now()
+        plaque.updated_by = None
+        print(plaque)
+
+        # Upload the image for a new plaque, or update the an editted plaque's image
+        ##### self._upload_image(img_name, img_fh, plaque)
+
+
+def _add_plaque_post() -> str:
+    plaque = _plaque_for_insert()
+    return _render_template(
+        "add.html",
+        plaques=[plaque],
+    )
+
+
+def _add_plaque_get() -> str:
+    maptext = (
+        "Click the plaque's location on the map, or search "
+        "for it, or enter its lat/lng location"
+    )
+    return _render_template(
+        "add.html",
+        plaques=None,
+        maptext=maptext,
+        mapzoom=10,
+        google_maps_api_key=get_key(),
+        page_title="Add Plaque",
+    )
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -159,23 +291,10 @@ def _plaque_for_insert(args) -> Plaque:
 @app.route("/submit-your-own", methods=["GET", "POST"])
 def add_plaque() -> str:
     if request.method == "POST":
-        return _render_template("add.html")
+        print("add_plaque POST")
+        return _add_plaque_post()
     elif request.method == "GET":
-        maptext = (
-            "Click the plaque's location on the map, or search "
-            "for it, or enter its lat/lng location"
-        )
-        return (
-            _render_template(
-                "add.html",
-                args_dict=dict(
-                    maptext=maptext,
-                    mapzoom=10,
-                    google_maps_api_key=get_key(),
-                    page_title="Add Plaque",
-                ),
-            ),
-        )
+        return _add_plaque_get()
     else:
         print("add not GET or POST")
         return redirect("/")
@@ -189,14 +308,21 @@ def edit_plaque(plaque_key: str) -> str:
     if request.method == "POST":
         return _render_template(
             "edit_p3_tmp.html",
-            args_dict=dict(
-                plaque_key=plaque_key,
-                title=request.form["title"],
-                tags=request.form["tags"],
-            ),
+            plaques=None,
+            plaque_key=plaque_key,
+            title=request.form["title"],
+            tags=request.form["tags"],
         )
     elif request.method == "GET":
-        return _render_template("edit_p3_tmp.html", plaque_key=plaque_key)
+        return _render_template(
+            "edit_p3_tmp.html",
+            plaques=None,
+            plaque_key=plaque_key,
+            maptext="",
+            mapzoom=9,
+            page_title="Edit Plaque",
+            message="Editing Plaque",
+        )
     else:
         return redirect("/")
 
@@ -267,7 +393,7 @@ def map(lat: str = None, lng: str = None, zoom: str = None) -> str:
             if zoom is not None:
                 template_values["bigmap_zoom"] = float(zoom)
 
-        return _render_template("bigmap.html", **template_values)
+        return _render_template("bigmap.html", plaques=None, **template_values)
 
 
 @app.route("/counts")
