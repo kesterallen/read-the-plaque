56233a661011ad5b7c05943a52ff96c0427d6812
Kester Allen <kester@gmail.com>
2024-01-26 17:03:42 -0800

    Incremental progress towards a working upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Main run script
 """
 
+import base64
 import datetime as dt
 import os
 import re
@@ -9,9 +10,16 @@ import random
 import urllib
 
 from google.cloud import ndb
+from google.cloud import storage
 from flask import Flask, render_template, request, redirect
 
 from rtp.models import Plaque, FeaturedPlaque
+
+# GCS_BUCKET configuration: This appears to work for the bucket named
+# 'read-the-plaque.appspot.com', Don't change this to, say, readtheplaque.com.
+#
+#GCS_BUCKET = "/read-the-plaque.appspot.com"
+GCS_BUCKET = "read-the-plaque.appspot.com"
 
 FIRST_YEAR = 2015
 FIRST_MONTH = 9
@@ -23,6 +31,11 @@ DEF_NUM_PENDING = 5
 DEF_NUM_RSS = 10
 DEF_NUM_PER_PAGE = 25
 DEF_RAND_NUM_PER_PAGE = 5
+
+
+class SubmitError(Exception):
+    pass
+
 
 # SET THIS envar TO GET CREDS:
 #
@@ -104,6 +117,10 @@ def pending_plaques(num: int = DEF_NUM_PENDING) -> str:
         return _render_template(
             "all.html",
             plaques=plaques,
+            bounding_box=_get_bounding_box(plaques),
+            mapzoom=10,
+            maptext="",
+            google_maps_api_key=get_key(),
             page_title="View Pending Plaques",
         )
 
@@ -115,12 +132,28 @@ def one_plaque(search_term: str) -> str:
     if request.method == "HEAD":
         return _render_template("head.html")
 
-    # TODO: map center/zoom needs to be set
     # TODO: add other search terms possibilities (key, old ID, etc)
     with ndb.Client().context() as context:
-        plaque = Plaque.query().filter(Plaque.title_url == search_term).get()
-        # print("key is ", plaque.key)
-        return _render_template("one.html", plaques=[plaque])
+        plaques = Plaque.query().filter(Plaque.title_url == search_term).fetch(1)
+        return _render_template(
+            "one.html",
+            plaques=plaques,
+            bounding_box=_get_bounding_box(plaques),
+            mapzoom=10,
+            maptext="",
+            google_maps_api_key=get_key(),
+        )
+
+
+def _get_bounding_box(plaques: list[Plaque]) -> list[float]:
+    """Calculate the bounding box corners for the coordinates of a list of plaques"""
+    if plaques:
+        lats = [p.location.latitude for p in plaques]
+        lngs = [p.location.longitude for p in plaques]
+        bounding_box = [[min(lngs), min(lats)], [max(lngs), max(lats)]]
+    else:
+        bounding_box = None
+    return bounding_box
 
 
 def _get_img_from_request():
@@ -166,59 +199,12 @@ def _latlng_angles_to_dec(ref, latlng_angles):
     return latlng
 
 
-def _get_latlng_exif(self, img_fh):
-    from PIL import Image
-    from PIL.ExifTags import TAGS, GPSTAGS
-
-    # Get GPS data out of image:
-    #
-    gps_data = {}
-    image = Image.open(img_fh)
-    info = image._getexif()
-    img_fh.seek(0)  # reset file handle
-    if info:
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == "GPSInfo":
-                for gps_tag in value:
-                    gps_tag_decoded = GPSTAGS.get(gps_tag, gps_tag)
-                    gps_data[gps_tag_decoded] = value[gps_tag]
-        try:
-            lat_ref = gps_data["GPSLatitudeRef"]  # N/S
-            lat_angles = _latlng_get_angles(gps_data["GPSLatitude"])
-
-            lng_ref = gps_data["GPSLongitudeRef"]  # E/W
-            lng_angles = _latlng_get_angles(gps_data["GPSLongitude"])
-
-            lat = _latlng_angles_to_dec(lat_ref, lat_angles)
-            lng = _latlng_angles_to_dec(lng_ref, lng_angles)
-        except KeyError:
-            pass  # TODO: is this right?
-
-        return lat, lng
-
-
-def _get_location(img_fh):
-    # If the location has been specified, use that:
-    lat = request.form["lat"]
-    lng = request.form["lng"]
-
-    # If it hasn't, but there's something in the search field, try that:
-    # TODO copy from AdminHandlers
-
-    # Convert to a location
-    # If that doesn't work, try to get the location from the image's EXIF info:
-    try:
-        location = ndb.GeoPt(lat, lng)
-    except ndb.exceptions.BadValueError as bve:
-        try:
-            lat, lng = self._get_latlng_exif(img_fh)
-            location = ndb.GeoPt(lat, lng)
-        except Exception as err:
-            print(err)
-            raise err
-
-    return location
+def _get_location():
+    """Convert form input into a GeoPt location"""
+    # TODO: verify lat / lng exist and are castable to float
+    lat = float(request.form["lat"])
+    lng = float(request.form["lng"])
+    return ndb.GeoPt(lat, lng)
 
 
 def _get_tags():
@@ -230,48 +216,96 @@ def _get_tags():
     return tags
 
 
+def _upload_image(img_name, img_fh, plaque):
+    """
+    Upload pic into GCS
+
+    The blobstore.create_gs_key and images.get_serving_url calls are
+    outside of the with block; I think this is correct. The
+    blobstore.create_gs_key call was erroring out on production when it was
+    inside the with block.
+
+    If gcs_fn is specified, overwrite that gcs filename. This is used
+    for updating the picture.
+    """
+    
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(GCS_BUCKET)
+    gcs_filename = dt.datetime.now().strftime('%Y%m%d/%H%M%S')
+    blob = bucket.blob(gcs_filename)
+
+    plaque.pic = gcs_filename
+
+    img_fh.seek(0) # reset file handle
+    base64_img_bytes = img_fh.read()
+    blob.upload_from_string(base64_img_bytes)
+    #blob.create()
+    blob.make_public()
+
+
+#    # Kill old image and URL, if they exist. If they don't, eat the error:
+#    #
+#    if plaque.pic is not None:
+#        try:
+#            storage.delete(plaque.pic)
+#        except:
+#            pass
+#    if plaque.img_url is not None:
+#        try:
+#            images.delete_serving_url(plaque.img_url)
+#        except:
+#            pass
+
+    #blob.upload_from_string(img_string)
+    print(blob.public_url)
+    print(blob.media_link)
+    plaque.img_url = blob.public_url
+
+
 def _plaque_for_insert() -> Plaque:
     """
     Make a new Plaque object for insert
     """
-    with ndb.Client().context() as context:
-        img_name, img_fh = _get_img_from_request()
+    img_name, img_fh = _get_img_from_request()
 
-        ### plaque_search_index = search.Index(PLAQUE_SEARCH_INDEX_NAME) # TODO figure out search in new GCP
-        ### plaque_search_index.put(plaque.to_search_document()) # TODO figure out search in new GCP
+    ### plaque_search_index = search.Index(PLAQUE_SEARCH_INDEX_NAME) # TODO figure out search in new GCP
+    ### plaque_search_index.put(plaque.to_search_document()) # TODO figure out search in new GCP
 
-        plaqueset_key = get_plaqueset_key()
-        plaque = Plaque(parent=plaqueset_key)
+    plaqueset_key = get_plaqueset_key()
+    plaque = Plaque(parent=plaqueset_key)
 
-        if request.form["title"] != plaque.title:
-            title = request.form["title"][:1499]  # limit to 1500 char
-            plaque.title = title
-            plaque.set_title_url(plaqueset_key)
+    plaque.title = request.form["title"][:1499]  # limit to 1500 char
+    plaque.set_title_url(plaqueset_key)
 
-        plaque.description = request.form["description"]
-        plaque.location = _get_location(img_fh)
-        plaque.tags = _get_tags()
-        plaque.approved = False
-        plaque.created_on = dt.datetime.now()
-        #        created_by = users.get_current_user() # TODO: figure out the users situation
-        # plaque.created_by = args.created_by  # ? created_by = users.get_current_user()
-        plaque.updated_on = dt.datetime.now()
-        plaque.updated_by = None
-        print(plaque)
+    plaque.description = request.form["description"]
+    plaque.location = _get_location()
+    plaque.tags = _get_tags()
+    plaque.approved = False
+    plaque.created_on = dt.datetime.now()
+    #        created_by = users.get_current_user() # TODO: figure out the users situation
+    # plaque.created_by = args.created_by  # ? created_by = users.get_current_user()
+    plaque.updated_on = dt.datetime.now()
+    plaque.updated_by = None
 
-        # Upload the image for a new plaque, or update the an editted plaque's image
-        ##### self._upload_image(img_name, img_fh, plaque)
+    # Upload the image for a new plaque, or update the an editted plaque's image
+    _upload_image(img_name, img_fh, plaque)
+    return plaque
 
 
 def _add_plaque_post() -> str:
-    plaque = _plaque_for_insert()
-    return _render_template(
-        "add.html",
-        plaques=[plaque],
-    )
+    """Do the POST request for /add"""
+    with ndb.Client().context() as context:
+        plaque = _plaque_for_insert()
+        plaque.put()  # ? TODO
+        return _render_template(
+            "add.html",
+            plaques=[plaque],
+            bounding_box=_get_bounding_box([plaque]),
+        )
 
 
 def _add_plaque_get() -> str:
+    """Render the page for the GET request to /add"""
     maptext = (
         "Click the plaque's location on the map, or search "
         "for it, or enter its lat/lng location"
@@ -290,18 +324,18 @@ def _add_plaque_get() -> str:
 @app.route("/submit", methods=["GET", "POST"])
 @app.route("/submit-your-own", methods=["GET", "POST"])
 def add_plaque() -> str:
+    """Add a new plaque"""
     if request.method == "POST":
-        print("add_plaque POST")
         return _add_plaque_post()
     elif request.method == "GET":
         return _add_plaque_get()
     else:
-        print("add not GET or POST")
         return redirect("/")
 
 
 @app.route("/edit/<string:plaque_key>", methods=["GET", "POST"])
 def edit_plaque(plaque_key: str) -> str:
+    """Add an existing plaque"""
     if plaque_key is None:
         return redirect("/")
 
